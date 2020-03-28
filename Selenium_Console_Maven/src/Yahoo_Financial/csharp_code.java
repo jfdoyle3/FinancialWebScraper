8aa4bb493db84aa0295274b2182910cabae2a5ef
Jim Doyle <indellibleink@gmail.com>
2020-03-27 23:34:39 -0400

    added C# login code to project. Need to refactor to work

---
+++ Selenium_Console_Maven/src/Yahoo_Financial/csharp_code.java
@@ -0,0 +1,50 @@
+package Yahoo_Financial;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.Keys;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.support.ui.WebDriverWait;
+import static org.openqa.selenium.support.ui.ExpectedConditions.presenceOfElementLocated;
+
+public class csharp_code {
+
+	public static void main(String[] args) {
+		
+	
+		 WebDriver driver = new FirefoxDriver();
+	        WebDriverWait wait = new WebDriverWait(driver,10);
+	    try {
+		 WebDriverWait waitSignIn = new WebDriverWait(driver, 10);
+         waitSignIn.Until(SeleniumExtras.WaitHelpers.ExpectedConditions.ElementToBeClickable(By.Id("uh-signedin")));
+
+         IWebElement signIn = driver.FindElement(By.Id("uh-signedin"));
+         signIn.Click();
+
+         WebDriverWait waitLogin = new WebDriverWait(driver,10);
+         waitLogin.Until(SeleniumExtras.WaitHelpers.ExpectedConditions.ElementToBeClickable(By.Id("login-username")));
+
+         IWebElement LoginField = driver.FindElement(By.Id("login-username"));
+         LoginField.SendKeys("jfdoyle_iii");
+         LoginField.SendKeys(Keys.Enter);
+
+         WebDriverWait waitPassword= new WebDriverWait(driver, TimeSpan.FromSeconds(20));
+         waitPassword.Until(SeleniumExtras.WaitHelpers.ExpectedConditions.ElementToBeClickable(By.Id("login-passwd")));
+
+
+         IWebElement passwordField = driver.FindElement(By.Id("login-passwd"));
+         passwordField.SendKeys("m93Fe8YHn");
+         passwordField.SendKeys(Keys.Enter);
+
+
+
+         driver.Navigate().GoToUrl("https://finance.yahoo.com/portfolio/p_2/view/v1");
+         WebDriverWait waitStockTable = new WebDriverWait(driver, TimeSpan.FromSeconds(10));
+         waitStockTable.Until(SeleniumExtras.WaitHelpers.ExpectedConditions.ElementExists(By.XPath("//table")));
+
+	        }finally {
+	            driver.quit();
+	        }
+	}
+}
