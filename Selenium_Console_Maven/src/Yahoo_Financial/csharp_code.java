a79bfa023b88322a81cfddf2c929f1f15d7410ff
Jim Doyle <indellibleink@gmail.com>
2020-03-28 07:22:33 -0400

    2020-03-28 07:22

--- Selenium_Console_Maven/src/Yahoo_Financial/csharp_code.java
+++ Selenium_Console_Maven/src/Yahoo_Financial/csharp_code.java
@@ -5,6 +5,7 @@ import org.openqa.selenium.Keys;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
 import static org.openqa.selenium.support.ui.ExpectedConditions.presenceOfElementLocated;
 
@@ -17,34 +18,37 @@ public class csharp_code {
 	        WebDriverWait wait = new WebDriverWait(driver,10);
 	    try {
 		 WebDriverWait waitSignIn = new WebDriverWait(driver, 10);
-         waitSignIn.Until(SeleniumExtras.WaitHelpers.ExpectedConditions.ElementToBeClickable(By.Id("uh-signedin")));
-
-         WebElement signIn = driver.FindElement(By.Id("uh-signedin"));
+         waitSignIn.until(ExpectedConditions.elementToBeClickable(By.id("uh-signedin")));
+         WebElement signIn = driver.findElement(By.id("uh-signedin"));
          signIn.click();
 
          WebDriverWait waitLogin = new WebDriverWait(driver,10);
-         waitLogin.Until(SeleniumExtras.WaitHelpers.ExpectedConditions.ElementToBeClickable(By.Id("login-username")));
+         waitLogin.until(ExpectedConditions.elementToBeClickable(By.id("login-username")));
 
-         WebElement LoginField = driver.FindElement(By.Id("login-username"));
+         WebElement LoginField = driver.findElement(By.id("login-username"));
          LoginField.sendKeys("jfdoyle_iii");
          LoginField.sendKeys(Keys.ENTER);
 
          WebDriverWait waitPassword= new WebDriverWait(driver, 20);
-         waitPassword.Until(SeleniumExtras.WaitHelpers.ExpectedConditions.ElementToBeClickable(By.Id("login-passwd")));
+         waitPassword.until(ExpectedConditions.elementToBeClickable(By.id("login-passwd")));
 
 
-         WebElement passwordField = driver.FindElement(By.Id("login-passwd"));
+         WebElement passwordField = driver.findElement(By.id("login-passwd"));
          passwordField.sendKeys("m93Fe8YHn");
          passwordField.sendKeys(Keys.ENTER);
 
 
 
-         driver.Navigate().GoToUrl("https://finance.yahoo.com/portfolio/p_2/view/v1");
-         WebDriverWait waitStockTable = new WebDriverWait(driver, TimeSpan.FromSeconds(10));
-         waitStockTable.Until(SeleniumExtras.WaitHelpers.ExpectedConditions.ElementExists(By.XPath("//table")));
+			/*
+			 * driver.navigate().GoToUrl("https://finance.yahoo.com/portfolio/p_2/view/v1");
+			 * WebDriverWait waitStockTable = new WebDriverWait(driver,10);
+			 * waitStockTable.Until(ExpectedConditions.ElementExists(By.xpath("//table")));
+			 */
 
 	        }finally {
 	            driver.quit();
 	        }
+	    
+	    System.out.println("successful");
 	}
 }
